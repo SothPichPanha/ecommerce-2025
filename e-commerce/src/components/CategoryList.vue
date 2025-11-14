0ebe628becf7ad672b45a7ad441d31577b0821e0
SothPichPanha <sothpichpanhaedu@gmail.com>
2025-11-14 17:00:30 +0700

    tp2

---
+++ e-commerce/src/components/CategoryList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="categories-wrapper">
+    <div class="categories">
+      <div
+        class="category-card"
+        v-for="(cat, index) in categories"
+        :key="index"
+        :style="{ backgroundColor: cat.color }"
+      >
+        <img :src="cat.image" :alt="cat.name" />
+        <h4>{{ cat.name }}</h4>
+        <p>{{ cat.productCount }} items</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryList",
+  props: {
+    categories: Array
+  }
+};
+</script>
+
+<style scoped>
+.categories-wrapper {
+ 
+  padding-bottom: 15px;
+}
+
+.categories {
+  display: flex;
+  gap: 20px;
+  min-width: max-content;
+}
+
+.category-card {
+  min-width: 110px;
+  text-align: center;
+  padding: 18px;
+  border-radius: 10px;
+  transition: 0.3s;
+  border: 1px solid #eee;
+}
+
+.category-card img {
+  width: 60px;
+  height: 60px;
+  margin-bottom: 8px;
+}
+
+.category-card h4 {
+  font-size: 14px;
+  font-weight: 600;
+}
+
+.category-card p {
+  font-size: 12px;
+  color: gray;
+}
+
+.category-card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 6px 20px rgba(0,0,0,0.1);
+}
+</style>
---
+++ e-commerce/src/components/PromotionList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="banners">
+    <div
+      class="banner-card"
+      v-for="(promo, index) in promotions"
+      :key="index"
+      :style="{ backgroundColor: promo.color }"
+    >
+      <div>
+        <h3>{{ promo.title }}</h3>
+        <button :style="{ backgroundColor: promo.buttonColor } " @click="shopnow(promo)"     >
+          Shop Now →
+        </button>
+      </div>
+
+      <img :src="promo.image" :alt="promo.title" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PromotionList",
+  props: { promotions: Array },
+  methods: {
+    shopnow(promo) {
+      alert("let's shop from: " + promo.title);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.banners {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+}
+
+.banner-card {
+  display: flex;
+  justify-content: space-between;
+  padding: 35px;
+  border-radius: 15px;
+  align-items: center;
+}
+
+.banner-card h3 {
+  width: 60%;
+  color: #1c1c1c;
+  line-height: 1.3;
+}
+
+.banner-card button {
+  color: #fff;
+  border: none;
+  padding: 8px 20px;
+  border-radius: 30px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.banner-card img {
+  width: 150px;
+  height: auto;
+}
+
+@media (max-width: 768px) {
+  .banners {
+    grid-template-columns: 1fr;
+  }
+}
+</style>

f232cfb9a14924dca4682e6d815bb583b25e59d1
SothPichPanha <sothpichpanhaedu@gmail.com>
2026-01-03 09:10:09 +0700

    fix

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Role;
+use App\Models\Permission;
 class RoleSeeder extends Seeder
 {
     /**
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
